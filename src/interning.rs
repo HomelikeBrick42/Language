f77c7ae862ab0b51651308a7ee6800c797fd4ac1
HomelikeBrick42 <lukevercoe555@gmail.com>
2024-08-29 16:02:13 +1200

    `InternedStr` no longer implements `Deref`

--- src/interning.rs
+++ src/interning.rs
@@ -1,32 +1,34 @@
 use derive_more::derive::{Debug, Display};
 use lasso::{Spur, ThreadedRodeo};
 use rustc_hash::FxBuildHasher;
-use std::{ops::Deref, sync::OnceLock};
+use std::sync::OnceLock;
 
 #[derive(Debug, Display, Clone, Copy, PartialEq, Eq, Hash)]
-#[display("{}", self as &str)]
-#[debug("{:?}", self as &str)]
+#[display("{}", self.to_str())]
+#[debug("{:?}", self.to_str())]
 pub struct InternedStr(Spur);
 
-impl From<&str> for InternedStr {
-    fn from(s: &str) -> Self {
+impl InternedStr {
+    pub fn intern(s: &str) -> Self {
         InternedStr(
             INTERNER
                 .get_or_init(|| ThreadedRodeo::with_hasher(FxBuildHasher))
                 .get_or_intern(s),
         )
     }
-}
 
-impl Deref for InternedStr {
-    type Target = str;
-
-    fn deref(&self) -> &Self::Target {
-        // SAFETY: if this type is constructed, then `INTERNER` has already been initialized by `Self::from`
+    pub fn to_str(&self) -> &'static str {
+        // SAFETY: if this type is constructed, then `INTERNER` has already been initialized by `Self::intern`
         let interner = unsafe { INTERNER.get().unwrap_unchecked() };
 
         // SAFETY: if this type has been constructed, then `self.0` was retrieved from `INTERNER` so it will be resolved
         unsafe { interner.try_resolve(&self.0).unwrap_unchecked() }
+    }
+}
+
+impl From<&str> for InternedStr {
+    fn from(s: &str) -> Self {
+        Self::intern(s)
     }
 }
 
