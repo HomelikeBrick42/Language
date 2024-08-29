0dbd212cb7b26c489433e799d80cf0b1901f4e32
HomelikeBrick42 <lukevercoe555@gmail.com>
2024-08-29 15:46:06 +1200

    Refactored `InternedStr` yet again

--- src/interning.rs
+++ src/interning.rs
@@ -1,20 +1,20 @@
 use derive_more::derive::{Debug, Display};
-use lasso::{Key, ThreadedRodeo};
+use lasso::{Spur, ThreadedRodeo};
 use rustc_hash::FxBuildHasher;
-use std::{num::NonZero, ops::Deref, sync::OnceLock};
-
-type IdType = u32;
+use std::{ops::Deref, sync::OnceLock};
 
 #[derive(Debug, Display, Clone, Copy, PartialEq, Eq, Hash)]
 #[display("{}", self as &str)]
 #[debug("{:?}", self as &str)]
-pub struct InternedStr(NonZero<IdType>);
+pub struct InternedStr(Spur);
 
 impl From<&str> for InternedStr {
     fn from(s: &str) -> Self {
-        INTERNER
-            .get_or_init(|| ThreadedRodeo::with_hasher(FxBuildHasher))
-            .get_or_intern(s)
+        InternedStr(
+            INTERNER
+                .get_or_init(|| InternerType::with_hasher(FxBuildHasher))
+                .get_or_intern(s),
+        )
     }
 }
 
@@ -22,22 +22,10 @@ impl Deref for InternedStr {
     type Target = str;
 
     fn deref(&self) -> &Self::Target {
-        // Safety: if this type is constructed then INTERNER should be initialized
-        unsafe { &INTERNER.get().unwrap_unchecked()[*self] }
+        // SAFETY: if this type is constructed, then `INTERNER` has already been initialized by `Self::from`
+        unsafe { INTERNER.get().unwrap_unchecked() }.resolve(&self.0)
     }
 }
 
-unsafe impl Key for InternedStr {
-    fn into_usize(self) -> usize {
-        // Safety: the only way this type is constructed is through `Self::try_from_usize`, so the value must fit in a usize
-        unsafe { (self.0.get() ^ IdType::MAX).try_into().unwrap_unchecked() }
-    }
-
-    fn try_from_usize(int: usize) -> Option<Self> {
-        Some(InternedStr(NonZero::new(
-            IdType::try_from(int).ok()? ^ IdType::MAX,
-        )?))
-    }
-}
-
-static INTERNER: OnceLock<ThreadedRodeo<InternedStr, FxBuildHasher>> = OnceLock::new();
+type InternerType = ThreadedRodeo<Spur, FxBuildHasher>;
+static INTERNER: OnceLock<InternerType> = OnceLock::new();
